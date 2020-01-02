6ac0d04fed657433a0a088027c93eaa3724e8e3c
guobo <1763446779@qq.com>
2020-01-02 16:23:09 +0800

    添加项目文件。

---
+++ MicroFramework.Core.Test/UnitTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MicroFramework.Core.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        public static void Test() {
+         var number= 0;
+        }
+    }
+}
